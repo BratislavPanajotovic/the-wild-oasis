a72cee0b5f6a6f114b5a6dd902ad620ffbe3448c
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2025-04-04 23:37:24 +0200

    handling errors in creating cabins

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -28,6 +28,16 @@ export async function createCabin(newCabin) {
     console.log(error);
     throw new Error("Cabins could not be created");
   }
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    throw new Error(
+      "Cabin image could not be uploaded and the cabin was not created"
+    );
+  }
 
   return data;
 }
